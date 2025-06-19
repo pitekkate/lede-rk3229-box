d5367efa20a725287c4161879b96075ad219d174
pitekkate <kangristiktok@gmail.com>
2025-06-19 16:01:02 +0700

    Update target.mk

--- target/linux/rockchip/armv7/target.mk
+++ target/linux/rockchip/armv7/target.mk
@@ -1,9 +1,9 @@
 ARCH:=arm
-SUBTARGET:=generic
+SUBTARGET:=armv7
 BOARDNAME:=Rockchip RK322x (ARMv7)
-CPU_TYPE:=cortex-a7
-CPU_SUBTYPE:=neon-vfpv4
-KERNELNAME:=zImage dtbs
+CPU_TYPE:=cortex-a9
+CPU_SUBTYPE:=vfpv3
+KERNELNAME:=Image dtbs
 
 define Target/Description
   Build images for $(BOARDNAME)
