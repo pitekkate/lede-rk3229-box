4cda746b628694ad2cb5758768f415dc1555f13d
pitekkate <kangristiktok@gmail.com>
2025-06-19 15:56:06 +0700

    Update target.mk

--- target/linux/rockchip/armv7/target.mk
+++ target/linux/rockchip/armv7/target.mk
@@ -1,10 +1,12 @@
-ARCH:=arm
-SUBTARGET:=generic
-BOARDNAME:=Rockchip RK322x (ARMv7)
-CPU_TYPE:=cortex-a7
-CPU_SUBTYPE:=neon-vfpv4
-KERNELNAME:=zImage dtbs
-
 define Target/Description
-  Build images for $(BOARDNAME)
+    Build for Rockchip ARMv7-based boards (RK3229, RK3228, etc.)
 endef
+
+TARGET_DEVICES += rk3229-evb
+DEVICE_TITLE.rk3229-evb := Rockchip RK3229 EVB
+DEVICE_PACKAGES.rk3229-evb := kmod-ssv6xxx firmware-ssv6158 \
+    kmod-usb-core kmod-usb-ehci kmod-usb-storage block-mount \
+    kmod-fs-ext4 kmod-scsi-cdrom kmod-scsi-generic \
+    kmod-ledtrig-netdev luci-app-opkg
+
+DEFAULT_PACKAGES += $(DEVICE_PACKAGES.rk3229-evb)
